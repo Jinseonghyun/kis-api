69abeb74619d8e7ea0122072d47d8b39ec9dfd5a
Jinseonghyun <jinsunghyun9@gmail.com>
2024-08-19 09:12:35 +0900

    프로젝트 세팅

---
+++ src/main/java/jin/kisapi/KisApiApplication.java
@@ -0,0 +1,13 @@
+package jin.kisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KisApiApplication.class, args);
+    }
+
+}

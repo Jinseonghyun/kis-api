5943f1096f68168cf1c9e417a20172f7806d1a32
Jinseonghyun <jinsunghyun9@gmail.com>
2024-08-19 13:47:37 +0900

    토큰관리 주석추가

--- src/main/java/jin/kisapi/config/AccessTokenManager.java
+++ src/main/java/jin/kisapi/config/AccessTokenManager.java
@@ -27,7 +27,7 @@ public class AccessTokenManager {
         return ACCESS_TOKEN;
     }
 
-
+    // Mono<TokenInfo>는 비동기적으로 HTTP POST 요청을 보내고, 응답을 TokenInfo 객체로 변환
     public String generateAccessToken() {
         String url = KisConfig.REST_BASE_URL + "/oauth2/tokenP";
         OauthInfo bodyOauthInfo = new OauthInfo();
@@ -35,14 +35,15 @@ public class AccessTokenManager {
         bodyOauthInfo.setAppkey(kisConfig.getAppKey());
         bodyOauthInfo.setAppsecret(kisConfig.getAppSecret());
 
+        // Mono는 비동기적으로 데이터를 처리하고, 0개 또는 1개의 결과를 반환하는 리액티브 타입
         Mono<TokenInfo> mono = webClient.post()
                 .uri(url)
                 .header("content-type", "application/json")
                 .bodyValue(bodyOauthInfo)
-                .retrieve()
+                .retrieve() // 응답을 비동기적으로 가져오는 메서드
                 .bodyToMono(TokenInfo.class);
 
-        TokenInfo tokenInfo = mono.block();
+        TokenInfo tokenInfo = mono.block(); // mono.block()는 Mono의 결과가 준비될 때까지 블로킹(대기)하고, 결과를 반환
         if (tokenInfo == null) {
             throw new RuntimeException("액세스 토큰을 가져올 수 없습니다.");
         }
